df208ebdc25d9f676dcd8d237a7f480575b7d31e
Rohit Jha <rohit305jha@gmail.com>
2013-06-27 17:25:10 +0530

    PE001 solved in C, Java, Haskell and Python

---
+++ Java/PE001.java
@@ -0,0 +1,52 @@
+/*
+
+Author: Rohit Jha
+File: PE001.java
+December 21, 2012
+
+Problem 1:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
+class PE001 {
+	static int sumDivisibleBy(int n) {
+		int limit = 999;
+		int p = limit / n;
+		return n * (p * (p + 1)) / 2;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15));
+	}
+}
+
+/*
+$ javac PE001.java
+$ /usr/bin/time -v java PE001
+233168
+	Command being timed: "java PE001"
+	User time (seconds): 0.05
+	System time (seconds): 0.01
+	Percent of CPU this job got: 97%
+	Elapsed (wall clock) time (h:mm:ss or m:ss): 0:00.07
+	Average shared text size (kbytes): 0
+	Average unshared data size (kbytes): 0
+	Average stack size (kbytes): 0
+	Average total size (kbytes): 0
+	Maximum resident set size (kbytes): 19268
+	Average resident set size (kbytes): 0
+	Major (requiring I/O) page faults: 0
+	Minor (reclaiming a frame) page faults: 2903
+	Voluntary context switches: 100
+	Involuntary context switches: 14
+	Swaps: 0
+	File system inputs: 0
+	File system outputs: 64
+	Socket messages sent: 0
+	Socket messages received: 0
+	Signals delivered: 0
+	Page size (bytes): 4096
+	Exit status: 0
+*/
---
+++ Python/PE001.py
@@ -0,0 +1,32 @@
+'''
+
+Author: Rohit Jha
+File: PE001.py
+June 23, 2012
+
+Problem 1:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+'''
+
+#! /usr/bin/env python
+def sumDivisibleBy(n):
+	p = 999 / n
+	return n * (p * (p + 1)) // 2
+
+def main():
+	print(int(sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15)))
+
+if __name__ == "__main__":
+	main()
+
+'''
+$ time python PE001.py
+233366
+
+real	0m0.041s
+user	0m0.037s
+sys	0m0.003s
+'''
